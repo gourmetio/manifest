7886869a62a5a8ce05b850b0e7198503faac805c
Max Jung <max@gourmetjs.com>
2020-08-13 15:18:39 -0700

    Add common.ts

---
+++ common.ts
@@ -0,0 +1,42 @@
+export interface Service {
+  name: string;
+  type: string;
+  props: Record<string, any>;
+}
+
+export interface BuildSpec {
+  name?: string;
+  builderImage?: string;
+  commands?: string[];
+  env?: EnvVars;
+  output?: BuildOutput;
+}
+
+export interface PathRewrite {
+  from: string;
+  to: string;
+}
+
+export type PathPattern = string | PathRewrite;
+
+export interface BuildOutput {
+  format?: "container" | "archive";
+  baseImage?: string;
+  files?: PathPattern[];
+}
+
+export type Build = BuildSpec | BuildSpec[];
+
+export interface Run {
+  command?: string[];
+  args?: string[];
+  env?: EnvVars;
+}
+
+export type EnvVars = Record<string, string | number>;
+
+export interface RootConfig {
+  workspaces?: string[];
+}
+
+export type ModuleExport = RootConfig | Service | ReadonlyArray<RootConfig | Service>;
--- nodejs.ts
+++ nodejs.ts
@@ -1,39 +1,4 @@
-export interface Service {
-  name: string;
-  type: string;
-  props: Record<string, any>;
-}
-
-export interface BuildSpec {
-  name?: string;
-  builderImage?: string;
-  commands?: string[];
-  env?: EnvVars;
-  output?: BuildOutput;
-}
-
-export interface PathRewrite {
-  from: string;
-  to: string;
-}
-
-export type PathPattern = string | PathRewrite;
-
-export interface BuildOutput {
-  format?: "container" | "archive";
-  baseImage?: string;
-  files?: PathPattern[];
-}
-
-export type Build = BuildSpec | BuildSpec[];
-
-export interface Run {
-  command?: string[];
-  args?: string[];
-  env?: EnvVars;
-}
-
-export type EnvVars = Record<string, string | number>;
+import {Service, Run, Build} from "./common.ts";
 
 export interface NodeService extends Service {
   type: "nodejs";
